243c0f7612a751d23bd35b2ea259f45ece92457e
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-04-25 17:22:57 -0500

    BE: Added Message Delete api route

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,18 @@
 const router = require('express').Router()
 module.exports = router
 
+const loggedInUser = (req, res, next) => {
+  if (req.user) {
+    next()
+  } else {
+    const newError = Error('Please login')
+    newError.code = 401
+    next(newError)
+  }
+}
+
 router.use('/users', require('./users'))
-router.use('/messages', require('./messages'))
+router.use('/messages', loggedInUser, require('./messages'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
--- server/api/messages.js
+++ server/api/messages.js
@@ -1,18 +1,8 @@
 const router = require('express').Router()
-const {User, Messages} = require('../db/models')
+const {Messages} = require('../db/models')
 module.exports = router
 
-const loggedInUser = (req, res, next) => {
-  if (req.user) {
-    next()
-  } else {
-    const newError = Error('Please login')
-    newError.code = 401
-    next(newError)
-  }
-}
-
-router.post('/', loggedInUser, async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
     const message = req.body
     const newMessage = await Messages.create({
@@ -26,29 +16,49 @@ router.post('/', loggedInUser, async (req, res, next) => {
   }
 })
 
-router.put('/:messageId', loggedInUser, async (req, res, next) => {
+router.put('/:messageId', async (req, res, next) => {
   try {
     const message = req.body
-    if (req.user !== message.userId) {
+    const messageToUpdate = await Messages.findByPk(req.params.messageId)
+    if (!messageToUpdate) {
+      const newError = Error(`No Message to update`)
+      newError.code = 400
+      throw newError
+    }
+    if (
+      req.user.id !== Number(message.userId) ||
+      messageToUpdate.userId !== Number(message.userId) ||
+      req.user.id !== messageToUpdate.userId
+    ) {
       const newError = Error('Not your Message. Can not update.')
       newError.code = 401
       throw newError
     }
-    const [count, rows] = await Messages.update(
-      {
-        text: message.text,
-        date: new Date(),
-        edited: true
-      },
-      {where: {id: req.params.messageId}}
-    )
-    if (count === 1) {
-      res.send(rows[0])
-    } else {
-      const newError = Error(`${count} rows were changed`)
+
+    messageToUpdate.text = message.text
+    messageToUpdate.date = new Date()
+    messageToUpdate.edited = true
+    await messageToUpdate.save()
+    res.send(messageToUpdate)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:messageId', async (req, res, next) => {
+  try {
+    const message = await Messages.findByPk(req.params.messageId)
+    if (!message) {
+      const newError = Error('Message not Found')
       newError.code = 400
       throw newError
+    } else if (req.user.id !== Number(message.userId)) {
+      const newError = Error('Not your Message. Can not delete')
+      newError.code = 401
+      throw newError
     }
+    await message.destroy()
+    res.sendStatus(204)
   } catch (error) {
     next(error)
   }

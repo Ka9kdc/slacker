dd7e7703105c77448273ef6a5be8ca334f9aae0f
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-04-25 17:23:48 -0500

    BE: added framework for backend message sockets

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,6 +2,20 @@ module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
+    socket.on('new message', message => {
+      console.log(message)
+      socket.broadcast('new message', message)
+    })
+
+    socket.on('update message', message => {
+      console.log(message)
+      socket.broadcast('update message', message)
+    })
+
+    socket.on('delete message', messageId => {
+      socket.broadcast('delete message', messageId)
+    })
+
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })

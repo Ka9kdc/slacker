0a0b60d4540fb8f47d03e0a11991d4ae7e121b2c
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-04-25 21:08:06 -0500

    FE: Add frontend socket connections for messages

--- client/socket.js
+++ client/socket.js
@@ -1,9 +1,25 @@
 import io from 'socket.io-client'
+import store from './store'
+import {addMessage, removeMessage, updateMessage} from './store/messages'
 
 const socket = io(window.location.origin)
 
 socket.on('connect', () => {
   console.log('Connected!')
+})
+
+socket.on('new message', message => {
+  console.log(message)
+  store.dispatch(addMessage(message))
+})
+
+socket.on('update message', message => {
+  console.log(message)
+  store.dispatch(updateMessage(message))
+})
+
+socket.on('delete message', messageId => {
+  store.dispatch(removeMessage(messageId))
 })
 
 export default socket
